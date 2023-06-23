daa1faef92771c0dd348b23a67fd4de5256d6b34
Zel-da <anjongman1029@gmail.com>
2023-06-23 16:47:21 +0900

    1.4

--- Create with Code/Prototype1/Assets/Scripts/PalyerController.cs
+++ Create with Code/Prototype1/Assets/Scripts/PalyerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PalyerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float fowardInput;
 
     void Start()
     {
@@ -13,7 +16,11 @@ public class PalyerController : MonoBehaviour
 
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
         //차량 전진
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
